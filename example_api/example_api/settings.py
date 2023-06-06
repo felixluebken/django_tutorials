ac57cc06b7b1ffd1213cac008ee906aaed04b33a
Felix Luebken <felixluebken2000@gmail.com>
2023-06-06 14:10:35 +0200

    disabled debug mode

--- example_api/example_api/settings.py
+++ example_api/example_api/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fj7akdk)z$-ku!c#y-p*)_u)cgiana0vrufr757(()kec+lui_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
